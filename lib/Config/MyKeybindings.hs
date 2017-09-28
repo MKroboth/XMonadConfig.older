f200d94554139f71b785ce1f74465dea42e98b7e
Maximilian Kroboth <mkr@cactis.eu>
2017-09-29 00:34:47 +0200

    keybindings reworked.

--- lib/Config/MyKeybindings.hs
+++ lib/Config/MyKeybindings.hs
@@ -5,30 +5,41 @@ import Config.XScreens
 import qualified XMonad.StackSet as W
 
 
-import qualified SpecialKeys
+import SpecialKeys
 
 (>>>) a b = (a,b)
 
 myKeybindings :: [(String, X ())]
 
 mpc x = safeSpawn "mpc" [x]
-mpcArg x i = safeSpawn "mpc" [x, i]
+mpc' x i = safeSpawn "mpc" [x, i]
 
 seekAmount = 5
 
+launchApplication :: String -> X ()
+launchApplication = flip safeSpawn $ []
+
+k x = "M-x " ++ x
+
+xscreenOrder = [2, 1, 0] -- was [0..] *** change to match your screen order ***
+
+workspaceKeys = [ (mask ++ "M-" ++ [key], screenWorkspace scr >>= flip whenJust (windows . action))
+                | (key, scr)  <- zip "wer" xscreenOrder
+                , (action, mask) <- [ (W.view, "") , (W.shift, "S-")]
+                ]
+
+applicationKeybindings = [ k"i j" >>> launchApplication "intellij-idea-ultimate-edition"
+                         , k"i c" >>> launchApplication "clion"
+                         , k"w"   >>> launchApplication "firefox"
+                         , k"e"   >>> launchApplication "emacs"
+                         ]
+
+musicControlKeybindings = [ xf86AudioPlay        >>> mpc  "toggle"
+                          , xf86AudioNext        >>> mpc  "next"
+                          , xf86AudioPrev        >>> mpc  "prev"
+                          , xf86AudioLowerVolume >>> mpc' "seek" ("-" ++ show seekAmount)
+                          , xf86AudioRaiseVolume >>> mpc' "seek" ("+" ++ show seekAmount)
+                          ]
+
 removeKeybindings = [ "M-shift-q" ]
-myKeybindings = [ "M-x i j" >>> safeSpawn "intellij-idea-ultimate-edition" []
-                , "M-x i c" >>> safeSpawn "clion" []
-                , "M-x w" >>> safeSpawn "google-chrome-stable" []
-                , "M-x e" >>> safeSpawn "emacs" []
-                , SpecialKeys.xf86AudioPlay  >>> mpc "toggle"
-                , SpecialKeys.xf86AudioNext  >>> mpc "next"
-                , SpecialKeys.xf86AudioPrev  >>> mpc "prev"
-                , SpecialKeys.xf86AudioLowerVolume >>> mpcArg "seek" ("-" ++ show seekAmount)
-                , SpecialKeys.xf86AudioRaiseVolume >>> mpcArg "seek" ("+" ++ show seekAmount)
-                ]
-                ++
-                [ (mask ++ "M-" ++ [key], screenWorkspace scr >>= flip whenJust (windows . action))
-                     | (key, scr)  <- zip "wer" [2, 1, 0] -- was [0..] *** change to match your screen order ***
-                     , (action, mask) <- [ (W.view, "") , (W.shift, "S-")]
-                ]
+myKeybindings = musicControlKeybindings ++ applicationKeybindings ++ workspaceKeys
