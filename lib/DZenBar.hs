7e245090b2119883dd1abb69234e3ecb20349d51
Maximilian Kroboth <mkr@cactis.eu>
2017-10-13 03:34:57 +0200

    applied some refactoring.

--- lib/DZenBar.hs
+++ lib/DZenBar.hs
@@ -3,6 +3,7 @@ module DZenBar where
 --     spawn "conky -c /home/mkr/.xmonad/config/leftConky.lua | dzen2 -e 'mouse2=' -xs 2 -h 18 -dock -ta r"
 
 import XMonad
+import Data.List
 
 x `piped` y = x ++ " | " ++ y
 
@@ -11,19 +12,19 @@ data DZenBarAlign = DLeft | DRight | DCenter
 data DZenBar = DZenBar
   { conkyConfigFile :: Maybe String
   , xscreen :: ScreenId
-  , position :: (Integer, Integer)
-  , width :: Integer
-  , height :: Integer
-  , align :: DZenBarAlign
+  , position :: Maybe (Integer, Integer)
+  , width :: Maybe Integer
+  , height :: Maybe Integer
+  , align :: Maybe DZenBarAlign
   }
 
 newDZenBar = DZenBar
   { conkyConfigFile = Nothing
   , xscreen = undefined
-  , position = undefined
-  , width = undefined
-  , height = undefined
-  , align = undefined
+  , position = Nothing
+  , width = Nothing
+  , height = Nothing
+  , align = Nothing
   }
 
 instance Show DZenBarAlign where
@@ -33,9 +34,30 @@ instance Show DZenBarAlign where
 
 instance Show DZenBar where
   show x = case conkyConfigFile x of
-    Just f ->  ("conky -c '" ++ f ++ "'")  `piped` dzen2Command
-    Nothing -> dzen2Command
-    where dzen2Command = "dzen2 -dock -e 'mouse2=' -xs " ++
-                             (let (S xs) = xscreen x in show (xs + 1)) ++
-                             " -x " ++ (show $ fst $ position x) ++ " -y " ++ (show $ snd $ position x) ++ " -w " ++ (show $ width x) ++ " -h " ++
-                             (show $ height x) ++ " -ta " ++ (show $ align x)
+              Just f ->  ("conky -c '" ++ f ++ "'")  `piped` dzen2Command
+              Nothing -> dzen2Command
+    where screenFlag = "-xs " ++ (let (S xs) = xscreen x in show (xs + 1))
+          xPositionFlag = case position x of
+                            Just pos -> "-x " ++ (show $ fst $ pos)
+                            Nothing -> ""
+          yPositionFlag = case position x of
+                            Just pos -> "-y " ++ (show $ snd $ pos)
+                            Nothing -> ""
+          widthFlag = case width x of
+                            Just w -> "-w " ++  (show $ w)
+                            Nothing -> ""
+          heightFlag = case height x of
+                            Just h -> "-h " ++ (show $ h)
+                            Nothing -> ""
+          alignFlag = case align x of
+                            Just a -> "-ta " ++ (show $ a)
+                            Nothing -> ""
+          dzen2Command = intercalate " " $ filter (not . null)
+                        [ "dzen2 -dock -e 'mouse2='"
+                        , screenFlag
+                        , xPositionFlag
+                        , yPositionFlag
+                        , widthFlag
+                        , heightFlag
+                        , alignFlag
+                        ]
--- lib/Hooks/MyStartupHook.hs
+++ lib/Hooks/MyStartupHook.hs
@@ -11,10 +11,6 @@ import Config.XScreens
 
 import System.Directory(getHomeDirectory)
 
-middle :: [a] -> [a]
-middle l@(_:_:_:_) = middle $ tail $ init l
-middle l           = l
-
 conkyConfig home x = home ++ "/.xmonad/config/conky/" ++ x
 
 menuBars :: FilePath -> [ScreenId] -> [DZenBar]
@@ -24,53 +20,51 @@ menuBars h screens = let
     config x = Just (conkyConfig h x)
     leftBar = newDZenBar
       { conkyConfigFile = config "leftConky.lua"
-      , xscreen = leftScreen
-      , position = (500, 0)
-      , width = 1420
-      , height = 18
-      , align = DRight
+      , xscreen         = leftScreen
+      , position        = Just (500, 0)
+      , width           = Just 1420
+      , height          = Just 18
+      , align           = Just DRight
       }
     uptimeBar = newDZenBar
       { conkyConfigFile = config "uptimeBar.lua"
-      , xscreen = leftScreen
-      , position = (0, 0)
-      , width = 200
-      , height = 18
-      , align = DLeft
+      , xscreen         = leftScreen
+      , position        = Just (0, 0)
+      , width           = Just 200
+      , height          = Just 18
+      , align           = Just DLeft
       }
     ramBar = newDZenBar
       { conkyConfigFile = config "ramBar.lua"
-      , xscreen = leftScreen
-      , position = (200, 0)
-      , width = 300
-      , height = 18
-      , align = DLeft
+      , xscreen         = leftScreen
+      , position        = Just (200, 0)
+      , width           = Just 300
+      , height          = Just 18
+      , align           = Just DLeft
       }
     rightBar = newDZenBar
       { conkyConfigFile = config "rightConky.lua"
-      , xscreen = rightScreen
-      , position = (0, 0)
-      , width = 1630
-      , height = 18
-      , align = DLeft
+      , xscreen         = rightScreen
+      , position        = Just (0, 0)
+      , width           = Just 1630
+      , height          = Just 18
+      , align           = Just DLeft
       }
     timeBar = newDZenBar
       { conkyConfigFile = config "timeBar.lua"
-      , xscreen = rightScreen
-      , position = (1630, 0)
-      , width = 200
-      , height = 18
-      , align = DRight
+      , xscreen         = rightScreen
+      , position        = Just (1630, 0)
+      , width           = Just 200
+      , height          = Just 18
+      , align           = Just DRight
       }
     in [leftBar, rightBar, uptimeBar, ramBar, timeBar]
 
 topBar screen = newDZenBar
-               { xscreen = screen
-               , position = (0, 0)
-               , width = 1920
-               , height = 18
-               , align = DLeft
-               }
+      { xscreen = screen
+      , height  = Just 18
+      , align   = Just DLeft
+      }
 
 myStartupHook :: X ()
 myStartupHook  = do
@@ -80,3 +74,6 @@ myStartupHook  = do
 
     sequence $ map (spawn . show) (menuBars home $ screens)
     spawn $ "stalonetray -c " ++ home ++ "/.xmonad/config/stalonetrayrc"
+  where middle :: [a] -> [a]
+        middle l@(_:_:_:_) = middle $ tail $ init l
+        middle l           = l
