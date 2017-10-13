5e7ff47f8aa21987e2bb79abaff9c7e2957dc16a
Maximilian Kroboth <mkr@cactis.eu>
2017-10-13 04:30:58 +0200

    more layouts added.

--- lib/Hooks/MyLayoutHook.hs
+++ lib/Hooks/MyLayoutHook.hs
@@ -2,7 +2,15 @@ module Hooks.MyLayoutHook(myLayoutHook) where
 
 import XMonad.Hooks.ManageDocks
 import XMonad
+import XMonad.Util.Themes
 import XMonad.Layout
-import XMonad.Layout.NoBorders(smartBorders)
+import XMonad.Layout.SimpleDecoration
+import XMonad.Layout.SimpleFloat
 
-myLayoutHook = smartBorders . avoidStruts $ layoutHook def
+import XMonad.Layout.NoBorders(smartBorders)
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Spiral
+
+
+myLayoutHook = smartBorders . avoidStruts $ layouts
+  where layouts = tabbed shrinkText (theme smallClean) ||| spiral(16/9) ||| layoutHook def
