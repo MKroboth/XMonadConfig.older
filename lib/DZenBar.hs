65e355f3499bcbadff676a0d69d74b6f82f09802
Maximilian Kroboth <mkr@cactis.eu>
2017-10-20 04:16:30 +0200

    .

--- lib/DZenBar.hs
+++ lib/DZenBar.hs
@@ -18,7 +18,7 @@ data DZenBar = DZenBar
   , align :: Maybe DZenBarAlign
   }
 
-newDZenBar = DZenBar
+newDZenBar = DZenBar--
   { conkyConfigFile = Nothing
   , xscreen = undefined
   , position = Nothing
@@ -36,20 +36,20 @@ instance Show DZenBar where
   show x = case conkyConfigFile x of
               Just f ->  ("conky -c '" ++ f ++ "'")  `piped` dzen2Command
               Nothing -> dzen2Command
-    where screenFlag = "-xs " ++ (let (S xs) = xscreen x in show (xs + 1))
+    where screenFlag = let (S xs) = xscreen x in "-xs " ++ show (xs + 1)
           xPositionFlag = case position x of
                             Just pos -> "-x " ++ (show $ fst $ pos)
                             Nothing -> ""
           yPositionFlag = case position x of
                             Just pos -> "-y " ++ (show $ snd $ pos)
                             Nothing -> ""
-          widthFlag = case width x of
+          widthFlag     = case width x of
                             Just w -> "-w " ++  (show $ w)
                             Nothing -> ""
-          heightFlag = case height x of
+          heightFlag    = case height x of
                             Just h -> "-h " ++ (show $ h)
                             Nothing -> ""
-          alignFlag = case align x of
+          alignFlag     = case align x of
                             Just a -> "-ta " ++ (show $ a)
                             Nothing -> ""
           dzen2Command = intercalate " " $ filter (not . null)
